2e516a1f59524ca5bd22baae7eb492198cc21982
wmike83 <wmike83@yahoo.com>
2014-07-09 16:07:20 -0400

    Finishing the History page binding and selection events

--- MountainBikeTracker_WP8/Views/HistoryPage.xaml.cs
+++ MountainBikeTracker_WP8/Views/HistoryPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using MountainBikeTracker_WP8.Models;
 
 namespace MountainBikeTracker_WP8.Views
 {
@@ -21,8 +22,8 @@ namespace MountainBikeTracker_WP8.Views
 
         private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            MessageBox.Show("here");
-            MessageBox.Show("uh");
+            MessageBox.Show("");
+            NavigationService.Navigate(new Uri("/Views/SaveCurrentRidePage.xaml?msg=SelectedIndex?ind=" + (lstActiveRiders.SelectedItem as Dictionary<string, TrailInformation>).Keys, UriKind.Relative));
         }
     }
 }
